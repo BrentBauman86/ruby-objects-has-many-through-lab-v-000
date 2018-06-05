64680dc00d2142ee465185131a5e42bd19916d19
Brent Bauman <brentbauman86@gmail.com>
2018-06-05 21:39:49 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,9 +19,9 @@ class Artist
     Song.new(name, self, genre)
   end
 
-  def songs
-    Song.all.select {|song| song.artist == self}
-  end
+  # def songs
+  #   Song.all.select {|song| song.artist == self}
+  # end
 
   def genres
     songs.collect {|song| song.genre}
